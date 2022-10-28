f9c7c4725a56d474749a825a498fa671ceb49ac5
Alex Torres <47037870+alextedtech@users.noreply.github.com>
2022-10-28 06:21:43 -0700

    Fixed save counter

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 let count = 0;
 let countEl = document.getElementById("count-el");
-let savenmb = document.getElementById("save-nmb");
+let saveEl = document.getElementById("save-el");
 
 function increment(){
-    count = count + 1;
-    countEl.innerText = count;
+    count += 1;
+    countEl.textContent = count;
 }
 
 function save(){
-
-
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    count = 0;
+    countEl.textContent = 0;
 }
 
