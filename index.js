18193ea1fe7ec554aa016fff3cac376a3fb86788
Alex Torres <47037870+alextedtech@users.noreply.github.com>
2022-10-28 05:42:51 -0700

    continue working on linking

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-document.getElementById("count-el").innerText = 5
+let count = 0;
+let countEl = document.getElementById("count-el");
+let savenmb = document.getElementById("save-nmb");
+
+function increment(){
+    count = count + 1;
+    countEl.innerText = count;
+}
+
+function save(){
+
+
+}
+
